73f30767b074c8ec6cf4a75979b95d2e4b2d0726
zmtssdy <382566258@qq.com>
2019-04-24 20:26:00 +0800

    index 改变

--- js/index.js
+++ js/index.js
@@ -1,9 +1,24 @@
 (function(){
+
+    // 首页登录 显示隐藏
     $('body').on('click', '.not-logged', function() {
-        $('.login-infor').show();
+        $('.login-infor').show()
     })
 
     $('body').on('click', '.login-close', function() {
-        $('.login-infor').hide();
+        $('.login-infor').hide()
+    })
+
+    // 页面头部双按钮 菜单切换
+    $('body').on('click', '.top-title ul li', function() {
+        $(this).addClass('active').siblings().removeClass('active')
+        var text_ = $(this).text()
+        if (text_ === "多媒体管理") {
+            $('.common-nav2').hide();
+            $('.common-nav1').show();
+        } else {
+            $('.common-nav1').hide();
+            $('.common-nav2').show();
+        }
     })
 })()
